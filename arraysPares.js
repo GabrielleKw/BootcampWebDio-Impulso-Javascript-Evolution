79d32342110284f12682a6030c4cb75e7fcdc478
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-22 15:54:09 -0300

    Create arraysPares.js

---
+++ arraysPares.js
@@ -0,0 +1,12 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+//esta certo
+var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+ 
+for (let i = 0; i < array.length ; i++){
+    if (array[i]%2 ==0 ){
+        print(array[ i ]);
+     }          
+}
